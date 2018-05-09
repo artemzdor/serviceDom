b0d6794ae576e8dadbde50e0db496ce479bebac9
artemzdor <artemzdor@gmail.com>
2018-05-09 04:23:30 +0300

    адрес по умолчанию

--- serviceDom.py
+++ serviceDom.py
@@ -539,4 +539,4 @@ def setSOSTZAKAZID(HASH, DECLID, SOSTZAKAZID):
         return "error"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
